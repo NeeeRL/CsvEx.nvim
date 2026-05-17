3d802806fc9a2ac1acf9aaa47456fd9e5dd4bd5d
NeeeRL <kenken.s5049@gmail.com>
2026-05-17 18:01:52 +0900

    docs: change command

--- lua/csvex/init.lua
+++ lua/csvex/init.lua
@@ -14,24 +14,29 @@ local function setup_highlights(opts)
 end
 
 function M.setup(opts)
-  require("csvex.config").setup(opts)
-  setup_highlights()
+  local config = require("csvex.config")
+  config.setup(opts)
+  setup_highlights(opts)
+
   local core = require("csvex.core")
+  local cfg = config.get()
 
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "csv",
     callback = function(args)
-      core.attach(args.buf)
+      if cfg.auto_attach then
+        core.attach(args.buf)
+      end
     end,
   })
 
-  if vim.bo.filetype == "csv" then
+  if cfg.auto_attach and vim.bo.filetype == "csv" then
     core.attach(vim.api.nvim_get_current_buf())
   end
+end
 
-  vim.api.nvim_create_user_command("CsvexTest", function()
-    core.attach(vim.api.nvim_get_current_buf())
-  end, {})
+function M.get()
+  return require("csvex.config").get()
 end
 
 return M
--- plugin/csvex.lua
+++ plugin/csvex.lua
@@ -1,3 +1,14 @@
-vim.api.nvim_create_user_command("CsvexTest", function()
-  require("csvex").setup()
+if vim.g.loaded_csvex_plugin then
+  return
+end
+vim.g.loaded_csvex_plugin = true
+
+vim.api.nvim_create_user_command("CsvexAttach", function()
+  local core = require("csvex.core")
+
+  if vim.bo.filetype == "csv" then
+    core.attach(vim.api.nvim_get_current_buf())
+  else
+    vim.notify("Not a CSV file", vim.log.levels.WARN)
+  end
 end, {})
